efed547304430b630f5e7073ec22990d0c81d668
Domingo Cabeza <domingoacd1@gmail.com>
2018-12-26 18:02:14 -0400

    Added loading spinner, comments, changed some colors. Better error handling.

--- script.js
+++ script.js
@@ -1,69 +1,145 @@
-const loadCategories = () => {
-
+const hideLoadSpinner = () => {
+    const loadSpinner = document.getElementById("load-spinner");
+    loadSpinner.classList.add("d-none");
 }
 
-const formSubmit = document.getElementById("api-form");
-const createCard = (element)=>{
+const showLoadSpinner = () => {
+    const loadSpinner = document.getElementById("load-spinner");
+    loadSpinner.classList.remove("d-none");
+}
+
+/* Creates the  container with the API info*/
+const createCard = (element) => {
     let card = document.createElement("div"),
         cardContent = document.createElement("div"),
         anchor = document.createElement("a"),
         cardTitle = document.createElement("h4"),
-        cardDescription = document.createElement("p");
+        cardDescription = document.createElement("p"),
+        resulstList = document.getElementById("result-list");
 
+    /* Creates the container for the API info */
     card.classList.add("card");
     card.classList.add("bg-primary");
+    card.classList.add("text-white");
     card.classList.add("mb-2");
     card.classList.add("mx-md-2");
     card.classList.add("col-11");
     card.classList.add("col-md-3");
-    document.getElementById("result-list").appendChild(card);
-    document.getElementById("result-list").classList.add("justify-content-center");
 
-    anchor.href = element.Link;
-    anchor.target = "blank"
-    anchor.classList.add("text-white");
-    card.appendChild(anchor);
+    resulstList.appendChild(card);
+    resulstList.classList.add("justify-content-center");
 
-    cardContent.classList.add("card-body");
-    anchor.appendChild(cardContent);
-    
-    cardTitle.textContent = element.API;
-    cardContent.appendChild(cardTitle);
+    if (element == null) {
+        /* If there are not API that match the search */
+        card.textContent = "No Results";
+    } else if (element == "error") {
+        /* If  the request failed*/
+        card.textContent = "Network error";
+    } else {
+        /* If the request was successful and there are matching APIs */
+        anchor.href = element.Link;
+        anchor.target = "blank"
+        anchor.classList.add("text-white");
+        card.appendChild(anchor);
 
-    cardDescription.textContent = element.Description;
-    cardContent.appendChild(cardDescription);
+        cardContent.classList.add("card-body");
+        anchor.appendChild(cardContent);
+
+        cardTitle.textContent = element.API;
+        cardContent.appendChild(cardTitle);
+
+        cardDescription.textContent = element.Description;
+        cardContent.appendChild(cardDescription);
+    }
 }
+
+/* If the connection with the server failed */
+const conectionError = () => {
+    hideLoadSpinner();
+    createCard("error");
+}
+
+/* Inserts the list of categories inside of its respective "select" field */
+const insertCategories = (categories) => {
+    const categoryInput = document.getElementById("category");
+    categories.forEach(category => {
+        let newOption = document.createElement("option");
+        newOption.value = category;
+        newOption.innerText = category;
+        categoryInput.appendChild(newOption);
+    });
+}
+
+/* Updates the list of APIs categories */
+const loadCategories = () => {
+    const request = new XMLHttpRequest();
+    const url = encodeURI('https://api.publicapis.org/categories');
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
+    request.onreadystatechange = () => {
+        if (request.status != 200) {
+            /* If request failed */
+            conectionError();
+        };
+    }
+    request.onload = ()=>{
+        /* If request was successful */
+        hideLoadSpinner();
+        insertCategories(request.response);
+    }
+}
+
+/* Clears the results of previous searches */
 const clearResults = ()=>{
     const results = document.getElementById("result-list");
     while(results.firstChild){
         results.removeChild(results.firstChild);
     }
 }
+
+/* Shows the query results */
 const showResults = (results)=>{
-    clearResults();
     let apis = results.entries;
-    for(let cont = 0; cont < apis.length; cont++){
-        createCard(apis[cont]);
+    if(apis == null){
+        /* If  no API match the search*/
+        createCard(apis);
+    }else{
+        for (let cont = 0; cont < apis.length; cont++) {
+            createCard(apis[cont]);
+        }
     }
 }
+
+/* Makes the request and receives the answer */
 const manageApisInfo = (event) => {
     event.preventDefault();
+    clearResults();
+    showLoadSpinner();
     const request = new XMLHttpRequest();
     const description = document.getElementById("desc").value;
     const authorization = document.getElementById("auth").value;
     const https = document.getElementById("https").value;
     const category = encodeURIComponent(document.getElementById("category").value);
     const url = `https://api.publicapis.org/entries?title=&description=${description}&auth=${authorization}&https=${https}&category=${category}`;
+    
     request.open('GET', url);
     request.responseType = 'json';
+    request.onreadystatechange = ()=>{
+        if(request.status != 200){
+            /* If request failed */
+            conectionError();
+        };
+    }
     request.send();
     request.onload = ()=>{
+        hideLoadSpinner();
         showResults(request.response);
     }
     
 }
+
+/* Events assignment section */
+window.onload = loadCategories;
+const formSubmit = document.getElementById("api-form");
 formSubmit.addEventListener("submit", manageApisInfo);
-const hideLoadBar = () => {
-    const loadSpinner = document.getElementById("load-spinner");
-    loadSpinner.classList.add("d-none");
-}
